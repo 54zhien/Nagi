85aefa1c7ddbe86071c1255d19c52652e4566b5e
imzhien <imzhien@users.noreply.github.com>
2026-08-24 02:07:08 +0800

    feat: 主页 tab 图标改用用户自定义图片 (homeIcon)

--- Seidoku/App/SeidokuApp.swift
+++ Seidoku/App/SeidokuApp.swift
@@ -25,7 +25,7 @@ struct RootTabView: View {
 
     var body: some View {
         TabView(selection: $selection) {
-            Tab("主页", systemImage: "house.fill", value: .home) {
+            Tab("主页", image: Image("homeIcon"), value: .home) {
                 HomeView()
             }
 
